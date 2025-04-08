1289d0e9539134be55767f95756c5bbf31807487
Hari Dass Sachdeva <135323298+hari7901@users.noreply.github.com>
2025-04-08 12:16:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,22 +19,7 @@ app.use(
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://client-expense-aqk8.vercel.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
 
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
 app.use(express.json());
 
 app.use((req, res, next) => {
