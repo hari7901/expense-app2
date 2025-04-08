8b99e95d5fd5a5b283548e313a163cd9cfbccd11
Hari <hariwork79@gmail.com>
2025-04-08 11:40:56 +0530

    Brief description of changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "../config/database.js";
-import expenseRoutes from "../routes/expenseRoutes.js";
+import connectDB from "./config/database.js";
+import expenseRoutes from "./routes/expenseRoutes.js";
 
 // Load environment variables
 dotenv.config();
