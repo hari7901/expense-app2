bdee46c9331fbdbf2139cbb5141c47c44f424102
Hari <hariwork79@gmail.com>
2025-04-08 11:20:04 +0530

    Brief description of changes

--- src/index.js
+++ src/index.js
@@ -14,13 +14,30 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000", // React development server
-      "http://127.0.0.1:3000", // Alternative localhost
+      "http://localhost:3000", 
+      "http://127.0.0.1:3000",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://client-expense-aqk8.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 app.use(express.json());
 
 app.use((req, res, next) => {
